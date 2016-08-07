43293e827af599f33bd1be49016e692087a41282
Ovidiu Angelescu <angelescuo@yahoo.com>
2016-08-07 10:09:26 +0300

    changes for lumina pkglist fuse and webcamd config scripts

--- packages/packages.cfg/fusefs-ext4fuse.sh
+++ packages/packages.cfg/fusefs-ext4fuse.sh
@@ -6,6 +6,13 @@ if [ $? -ne 0 ]; then
     echo 'fuse_load="YES"' >> /boot/loader.conf
 fi
 
+grep -q "fuse" /boot/grub/grub.cfg
+if [ $? -ne 0 ]; then
+    sed -i '' '/set kFreeBSD.kern.vty=vt/a\
+\  kfreebsd_module_elf /boot/kernel/fuse.ko\
+' /boot/grub/grub.cfg
+fi
+
 if [ -e /usr/local/bin/ext4fuse ]; then
     ln -s /usr/local/bin/ext4fuse /sbin/mount_ext4fs
 fi
--- packages/packages.cfg/fusefs-ntfs.sh
+++ packages/packages.cfg/fusefs-ntfs.sh
@@ -4,6 +4,13 @@
 grep -q "fuse_load" /boot/loader.conf
 if [ $? -ne 0 ]; then
     echo 'fuse_load="YES"' >> /boot/loader.conf
+fi
+
+grep -q "fuse" /boot/grub/grub.cfg
+if [ $? -ne 0 ]; then
+    sed -i '' '/set kFreeBSD.kern.vty=vt/a\
+\  kfreebsd_module_elf /boot/kernel/fuse.ko\
+' /boot/grub/grub.cfg
 fi 
 
 if [ -e /usr/local/bin/ntfs-3g ]; then
